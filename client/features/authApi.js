332f5eef5320dddff0655f9e176c75d829608e32
abhishek <110403133+AbhishekBagal9@users.noreply.github.com>
2025-09-05 22:39:55 +0530

    deployment issue

--- client/features/authApi.js
+++ client/features/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v1/user",
+    baseUrl:process.env.API_URL,
     credentials: "include",
   }),
   endpoints: (builder) => ({
